86f3db4ffad241c2d98aa42184d0b7d40df72e54
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-07-25 12:24:12 +0530

    Add siddhi app manager

--- app/src/main/java/org/wso2/siddhiservice/AppManager.java
+++ app/src/main/java/org/wso2/siddhiservice/AppManager.java
@@ -1,13 +1,31 @@
 package org.wso2.siddhiservice;
 
+import org.wso2.siddhi.core.SiddhiAppRuntime;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.query.compiler.SiddhiQLParser;
+import org.wso2.siddhiservice.output.BroadcastIntentSink;
+import org.wso2.siddhiservice.sensors.proximity.ProximitySensorSource;
+
+import java.util.HashMap;
+
 /**
  * Created by chamath on 7/25/17.
  */
 
 public class AppManager {
 
+    private SiddhiManager siddhiManager;
+    private HashMap<String,SiddhiAppRuntime> siddhiAppList;
 
+    public AppManager(){
+        siddhiAppList=new HashMap<>();
+        this.siddhiManager=new SiddhiManager();
+        siddhiManager.setExtension("source:proximity",ProximitySensorSource.class);
+        siddhiManager.setExtension("sink:broadcast", BroadcastIntentSink.class);
+    }
 
-
-
+    public void startApp(String inStream,String identifier){
+        SiddhiAppRuntime siddhiAppRuntime=siddhiManager.createSiddhiAppRuntime(inStream);
+        siddhiAppRuntime.start();
+    }
 }
--- app/src/main/java/org/wso2/siddhiservice/SiddhiAppService.java
+++ app/src/main/java/org/wso2/siddhiservice/SiddhiAppService.java
@@ -5,23 +5,19 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
-import org.wso2.siddhi.core.SiddhiAppRuntime;
-import org.wso2.siddhi.core.SiddhiManager;
-import org.wso2.siddhiservice.output.BroadcastIntentSink;
-import org.wso2.siddhiservice.sensors.proximity.ProximitySensor;
-import org.wso2.siddhiservice.sensors.proximity.ProximitySensorSource;
 
 public class SiddhiAppService extends Service {
     private RequestController requestController=new RequestController();
     public static SiddhiAppService instance;
+    private AppManager appManager;
     public SiddhiAppService() {
         SiddhiAppService.instance=this;
+        appManager=new AppManager();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e("Service","Start the service");
-
         return START_STICKY;
     }
 
@@ -32,40 +28,18 @@ public class SiddhiAppService extends Service {
     }
 
 
-    private void runSiddhiApp(){
-
-        SiddhiManager siddhiManager = new SiddhiManager();
-        siddhiManager.setExtension("source:proximity",ProximitySensorSource.class);
-        siddhiManager.setExtension("sink:broadcast", BroadcastIntentSink.class);
-        String inStreamDefinition = "" +
-                "@app:name('foo')" +
-                "@source(type='proximity',classname='org.wso2.ceptest.MainActivity', @map(type='passThrough'))" +
-                "@sink(type='broadcast',@map(type='passThrough'))" +
-                "define stream streamProximity ( sensorName string, timestamp long, accuracy int,distance float);";
-
-
-        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition);
-
-        siddhiAppRuntime.start();
-
-    }
-
     private class RequestController extends IRequestController.Stub{
 
         @Override
         public String startSiddhiApp(String definition, String identifier) throws RemoteException {
             Log.e("App","Start Siddhi App New TWO");
-            Runnable r=new Runnable() {
-                @Override
-                public void run() {
-                    runSiddhiApp();
-                }
+            Runnable r= () -> {
+                appManager.startApp(definition,identifier);
             };
-
             Thread t=new Thread(r);
             t.setContextClassLoader(this.getClass().getClassLoader());
             t.start();
-            return "Hello World";
+            return "App Launched";
         }
     }
 }
