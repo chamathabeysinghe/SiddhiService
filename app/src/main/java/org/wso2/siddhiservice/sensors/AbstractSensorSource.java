02cbed9db5a2e9b825b30b7f42315fca40aa5af9
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-07-26 16:50:28 +0530

    Working 3

--- app/src/main/java/org/wso2/siddhiservice/sensors/AbstractSensorSource.java
+++ app/src/main/java/org/wso2/siddhiservice/sensors/AbstractSensorSource.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.siddhiservice.sensors;
-
-import android.hardware.Sensor;
 
 import org.wso2.siddhi.core.config.SiddhiAppContext;
 import org.wso2.siddhi.core.event.Event;
@@ -13,9 +28,6 @@ import org.wso2.siddhi.query.api.definition.StreamDefinition;
 
 import java.util.Map;
 
-/**
- * Created by chamath on 7/26/17.
- */
 
 public abstract class AbstractSensorSource extends Source{
 
