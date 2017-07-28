98a996c4621b9233905c17ceb50598874c5db506
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-07-28 13:51:42 +0530

    Change UI

--- app/src/main/java/org/wso2/siddhiservice/output/BroadcastIntent.java
+++ app/src/main/java/org/wso2/siddhiservice/output/BroadcastIntent.java
@@ -19,9 +19,6 @@ package org.wso2.siddhiservice.output;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 
 import org.wso2.siddhiservice.SiddhiAppService;
 
