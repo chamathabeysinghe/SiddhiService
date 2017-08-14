0ecb5d9425f00c0838eba7264ca4f5c121483908
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-08-14 12:27:20 +0530

    Add pressure sensor source

--- app/src/main/java/org/wso2/siddhiservice/sensors/PressureSensorSource.java
+++ app/src/main/java/org/wso2/siddhiservice/sensors/PressureSensorSource.java
@@ -1,23 +1,4 @@
-/*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- * WSO2 Inc. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.wso2.siddhiservice.sensors;
-
-
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -29,23 +10,22 @@ import org.wso2.siddhi.core.stream.input.source.SourceEventListener;
 import org.wso2.siddhi.core.util.config.ConfigReader;
 import org.wso2.siddhi.core.util.transport.OptionHolder;
 
-
 @Extension(
-        name = "proximity",
+        name = "pressure",
         namespace="source",
-        description = "Get events from the ambient temperature sensor",
+        description = "Get events from the light sensor",
         examples = @Example(description = "TBD",syntax = "TBD")
 )
-public class ProximitySensorSource extends AbstractSensorSource {
+public class PressureSensorSource extends AbstractSensorSource {
 
-    private float previousValue=-275;
+    private float previousValue=-1;
     @Override
     public void init(SourceEventListener sourceEventListener, OptionHolder optionHolder, String[] strings, ConfigReader configReader, SiddhiAppContext siddhiAppContext) {
         super.init(sourceEventListener,optionHolder,strings,configReader,siddhiAppContext);
-        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        if(sensor==null)
-            Log.e("Siddhi Source Error","Light Sensor is not supported in the device. Stream "+sourceEventListener.getStreamDefinition().getId());
 
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if(sensor==null)
+            Log.e("Siddhi Source Error","Pressure Sensor is not supported in the device. Stream "+sourceEventListener.getStreamDefinition().getId());
     }
 
     @Override
@@ -59,10 +39,12 @@ public class ProximitySensorSource extends AbstractSensorSource {
         if(event.values[0]==previousValue)
             return;
         previousValue=event.values[0];
+
         Object eventOutput[] ={event.sensor.getName(),event.timestamp,event.accuracy,event.values[0]};
 
 
         this.sourceEventListener.onEvent(eventOutput,null);
+
     }
 
     @Override
