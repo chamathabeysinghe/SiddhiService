34ff3ad08e46bbc6028cc576bfa885ad378d1ef0
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-08-14 13:56:25 +0530

    Tested Sources

---
+++ app/src/main/java/org/wso2/siddhiservice/sensors/HumiditySensorSource.java
@@ -0,0 +1,55 @@
+package org.wso2.siddhiservice.sensors;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.util.Log;
+
+import org.wso2.siddhi.annotation.Example;
+import org.wso2.siddhi.annotation.Extension;
+import org.wso2.siddhi.core.config.SiddhiAppContext;
+import org.wso2.siddhi.core.stream.input.source.SourceEventListener;
+import org.wso2.siddhi.core.util.config.ConfigReader;
+import org.wso2.siddhi.core.util.transport.OptionHolder;
+
+@Extension(
+        name = "humidity",
+        namespace="source",
+        description = "Get events from the light sensor",
+        examples = @Example(description = "TBD",syntax = "TBD")
+)
+public class HumiditySensorSource extends AbstractSensorSource {
+
+    private float previousValue=-1;
+    @Override
+    public void init(SourceEventListener sourceEventListener, OptionHolder optionHolder, String[] strings, ConfigReader configReader, SiddhiAppContext siddhiAppContext) {
+        super.init(sourceEventListener,optionHolder,strings,configReader,siddhiAppContext);
+
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        if(sensor==null)
+            Log.e("Siddhi Source Error","Humidity Sensor is not supported in the device. Stream "+sourceEventListener.getStreamDefinition().getId());
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        sensor=null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.values[0]==previousValue)
+            return;
+        previousValue=event.values[0];
+
+        Object eventOutput[] ={event.sensor.getName(),event.timestamp,event.accuracy,event.values[0]};
+
+
+        this.sourceEventListener.onEvent(eventOutput,null);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
